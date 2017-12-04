8fd3f54891881e66dfd4d6187bbbef67b929c7c3
AlexisAMAND <alexis.amand@gmail.com>
2017-12-04 14:54:12 +0100

    Première mise en ligne du projet

---
+++ The Gasp/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(158, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(140, 130)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(302, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(140, 130)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(448, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(140, 130)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(10, 149)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(140, 130)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(156, 149)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(140, 130)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(302, 148)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(140, 130)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(448, 149)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(140, 130)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 285)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(140, 130)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(156, 285)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(140, 130)
+        Me.Button10.TabIndex = 9
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(304, 284)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(140, 130)
+        Me.Button11.TabIndex = 10
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(450, 285)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(140, 130)
+        Me.Button12.TabIndex = 11
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(12, 421)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(140, 130)
+        Me.Button13.TabIndex = 12
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(158, 421)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(140, 130)
+        Me.Button14.TabIndex = 13
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(304, 420)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(140, 130)
+        Me.Button15.TabIndex = 14
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(450, 421)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(140, 130)
+        Me.Button16.TabIndex = 15
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Location = New System.Drawing.Point(6, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(181, 114)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Label1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(10, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 130)
+        Me.Button1.TabIndex = 18
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(604, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(193, 139)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(604, 384)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(193, 50)
+        Me.Button17.TabIndex = 20
+        Me.Button17.Text = "Button17"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(604, 440)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(193, 50)
+        Me.Button18.TabIndex = 21
+        Me.Button18.Text = "Button18"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label2.Location = New System.Drawing.Point(601, 536)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(196, 14)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Label2"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.The_Gasp.My.Resources.Resource1.fond
+        Me.ClientSize = New System.Drawing.Size(809, 563)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ The Gasp/Form1.vb
@@ -0,0 +1,284 @@
+﻿Public Class Form1
+
+    Dim version = "0.1.0" 'version du jeu
+
+    Dim Damier(5, 5) As Char 'le damier 
+    Dim NombreCoups = 0 'nombre de coups 
+
+    ' coordonnées du pion
+
+    Dim xbouton As Integer
+    Dim ybouton As Integer
+
+    ' Cette fonctione retourne les pions
+
+    Private Sub retourne(xb, yb)
+
+        For l = (yb - 1) To (yb + 1)
+            For c = (xb - 1) To (xb + 1)
+                If Damier(c, l) = "X" Then
+                    Damier(c, l) = "0"
+                Else
+                    Damier(c, l) = "X"
+                End If
+            Next
+        Next
+
+        If Damier(xb, yb) = "X" Then
+            Damier(xb, yb) = "0"
+        Else
+            Damier(xb, yb) = "X"
+        End If
+
+        NombreCoups = NombreCoups + 1
+        Label1.Text = NombreCoups
+
+        afficher()
+
+    End Sub
+
+    ' Cette fonction affiche les pions
+
+    Private Sub afficher()
+
+        ' afficher les pions de la 1ere ligne
+
+        For i = 1 To 4
+            If Damier(i, 1) = "X" Then
+                Controls("Button" & (i).ToString).BackColor = Color.Sienna
+                'Controls("Button" & i.ToString).BackgroundImage = Image.FromFile("E:\Dev\Visual Studio 2017\Projects\Le Gasp\Ressources\noir.png")
+                'Controls("Button" & i.ToString).BackgroundImageLayout = ImageLayout.Stretch
+            Else
+                Controls("Button" & (i).ToString).BackColor = Color.PeachPuff
+                'Controls("Button" & i.ToString).BackgroundImage = Image.FromFile("E:\Dev\Visual Studio 2017\Projects\Le Gasp\Ressources\blanc.png")
+                'Controls("Button" & i.ToString).BackgroundImageLayout = ImageLayout.Stretch
+            End If
+        Next
+
+        ' afficher les pions de la 2e ligne
+
+        For i = 1 To 4
+            If Damier(i, 2) = "X" Then
+                Controls("Button" & (i + 4).ToString).BackColor = Color.Sienna
+            Else
+                Controls("Button" & (i + 4).ToString).BackColor = Color.PeachPuff
+            End If
+        Next
+
+        ' afficher les pions de la 3e ligne
+
+        For i = 1 To 4
+            If Damier(i, 3) = "X" Then
+                Controls("Button" & (i + 8).ToString).BackColor = Color.Sienna
+            Else
+                Controls("Button" & (i + 8).ToString).BackColor = Color.PeachPuff
+            End If
+        Next
+
+        ' afficher les pions de la 4e ligne
+
+        For i = 1 To 4
+            If Damier(i, 4) = "X" Then
+                Controls("Button" & (i + 12).ToString).BackColor = Color.Sienna
+            Else
+                Controls("Button" & (i + 12).ToString).BackColor = Color.PeachPuff
+            End If
+        Next
+
+    End Sub
+
+    ' Cette fonction lance une nouvelle partie
+
+    Private Sub nouvelle()
+
+        NombreCoups = 0
+
+        For c = 0 To 5
+            For l = 0 To 5
+                Damier(c, l) = "X"
+            Next
+        Next
+
+        Label1.Text = NombreCoups
+
+        afficher()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "The Gasp"
+        Label2.Text = "Version " & version
+
+        MaximizeBox = vbFalse
+        MinimizeBox = vbFalse
+
+        Button17.Text = "Nouvelle partie"
+        Button18.Text = "Quitter"
+        GroupBox1.Text = "Nombre de coups"
+
+        nouvelle()
+
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        xbouton = 2
+        ybouton = 1
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        xbouton = 3
+        ybouton = 1
+
+        retourne(xbouton, ybouton)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        xbouton = 4
+        ybouton = 1
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        xbouton = 1
+        ybouton = 2
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        xbouton = 2
+        ybouton = 2
+
+        retourne(xbouton, ybouton)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        xbouton = 3
+        ybouton = 2
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+
+        xbouton = 4
+        ybouton = 2
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+        xbouton = 1
+        ybouton = 3
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs)
+
+        nouvelle()
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+
+        xbouton = 2
+        ybouton = 3
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        xbouton = 3
+        ybouton = 3
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+        xbouton = 4
+        ybouton = 3
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+
+        xbouton = 1
+        ybouton = 4
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+
+        xbouton = 2
+        ybouton = 4
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+
+        xbouton = 3
+        ybouton = 4
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+
+        xbouton = 4
+        ybouton = 4
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        xbouton = 1
+        ybouton = 1
+
+        retourne(xbouton, ybouton)
+
+    End Sub
+
+    Private Sub Button17_Click_1(sender As Object, e As EventArgs) Handles Button17.Click
+
+        nouvelle()
+
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+
+        Me.Close()
+
+    End Sub
+End Class
